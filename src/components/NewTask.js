216b628231219dcc033e19e8380f56b1a2252a1e
Rodrigo Giraldi Silva <rgiraldisilva@gmail.com>
2019-07-25 23:30:26 -0300

    Created ajax to new task

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 
-import { isLoggedIn } from '../services/SessionService';
+import { isLoggedIn, getToken } from '../services/SessionService';
 
 export default class NewTask extends Component {
 
     constructor(props) {
         super(props);
 
-        this.state = { categories: [] };
+        this.state = {
+            categories: [],
+            category: "",
+            startDate: "",
+            startTime: "",
+            endDate: "",
+            endTime: "",
+            description: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentWillMount() {
@@ -25,6 +36,48 @@ export default class NewTask extends Component {
         }
     }
 
+    handleChange(event) {
+        let name = event.target.name;
+        let value = event.target.value;
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        if (this.isFormValid()) {
+            let objectToSubmit = {
+                id: 0,
+                category: this.state.category,
+                startDateTime: new Date(`${this.state.startDate} ${this.state.startTime}`),
+                endDateTime: new Date(`${this.state.endDate} ${this.state.endTime}`),
+                description: this.state.description
+            }
+
+            fetch("http://localhost:8080/task",
+                {
+                    method: 'post',
+                    body: JSON.stringify(objectToSubmit),
+                    headers: {
+                        'Authorization': getToken(),
+                        'Content-Type': 'application/json'
+                    }
+                })
+                .then(res => res.json()
+                    .then(resJson => {
+                        this.props.history.push('/search-task');
+                    }));
+
+        }
+        else {
+            console.log("You have to fill in all the fields to create a task");
+        }
+    }
+
+    isFormValid() {
+        return (this.state.category !== "" && this.state.startDate !== "" && this.state.startTime !== "" && this.state.endDate !== "" && this.state.endTime !== "" && this.state.description !== "");
+    }
+
     render() {
         return (
             <div className="container">
@@ -33,10 +86,10 @@ export default class NewTask extends Component {
                         <h3 className="mt-4">
                             <strong>New Task</strong>
                         </h3>
-                        <form>
+                        <form onSubmit={this.handleSubmit}>
                             <div className="form-group">
                                 <label htmlFor="category">Category</label>
-                                <select className="form-control" name="category">
+                                <select className="form-control" name="category" onChange={this.handleChange}>
                                     {this.state.categories.map(category =>
                                         <option key={category}>{category}</option>
                                     )}
@@ -45,27 +98,27 @@ export default class NewTask extends Component {
                             <div className="form-row">
                                 <div className="form-group col-md-6">
                                     <label htmlFor="inputStartDate">Start date</label>
-                                    <input name="startDate" type="date" className="form-control" id="inputStartDate" />
+                                    <input name="startDate" type="date" className="form-control" id="inputStartDate" onChange={this.handleChange} />
                                 </div>
                                 <div className="form-group col-md-6">
                                     <label htmlFor="inputStartTime">Time</label>
-                                    <input name="startTime" type="time" className="form-control" id="inputStartTime" />
+                                    <input name="startTime" type="time" className="form-control" id="inputStartTime" onChange={this.handleChange} />
                                 </div>
                             </div>
                             <div className="form-row">
                                 <div className="form-group col-md-6">
                                     <label htmlFor="inputEndDate">End date</label>
-                                    <input name="startEnd" type="date" className="form-control" id="inputEndDate" />
+                                    <input name="endDate" type="date" className="form-control" id="inputEndDate" onChange={this.handleChange} />
                                 </div>
                                 <div className="form-group col-md-6">
                                     <label htmlFor="inputEndTime">Time</label>
-                                    <input name="endTime" type="time" className="form-control" id="inputEndTime" />
+                                    <input name="endTime" type="time" className="form-control" id="inputEndTime" onChange={this.handleChange} />
                                 </div>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="inputDescription">Description</label>
                                 <input name="description" type="text" className="form-control" id="inputDescription"
-                                    placeholder="Enter description for this task" />
+                                    placeholder="Enter description for this task" onChange={this.handleChange} />
                             </div>
                             <button className="btn btn-primary">Create</button>
                         </form>
